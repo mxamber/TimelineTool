a0432a58b625e3b53fce93bf84aecd624fefbc60
Amber Alex <13611718+mxamber@users.noreply.github.com>
2024-09-30 01:34:20 +0200

    Revamp 2024
    New features:
    - time spans
    - timeline title
    - minor refactoring
    - improved sorting

--- draw.js
+++ draw.js
@@ -1,10 +1,13 @@
+const GLOBAL_OFFSET_STEP = 50;
+
 // create default timeline
 var timeline = {
 	padding: { top: 100, right: 0, bottom: 50, left: 50 },
 	zoom: 1,
 	offset: 2000,
 	end: 2020,
-	events: []
+	events: [],
+	timespans: []
 };
 
 // simple shorthand for getElementById() and querySelector()
@@ -12,6 +15,36 @@ function elem(ident) {
 	let element = document.getElementById(ident);
 	(element == null || element == undefined) ? element = document.querySelector(ident) : element = element;
 	return element;
+}
+
+// calculate average color intensity for an RGB color (because I'm too lazy to do proper HSV conversion)
+function colorStrength(color) {
+
+	// if the color is hexadecimal, convert to RGB before proceeding
+	if(typeof color == "string" && color.startsWith("#")) {
+		
+		// if the string is too short to be proper hex RGB, return false
+		if(color.length < 7) { return false; }
+		
+		// cut the # sign and extract 16-bit R, G, and B components
+		color = color.substr(1);
+		let r = color.substr(0,2);
+		let g = color.substr(2,2);
+		let b = color.substr(4,2);
+		
+		// convert to integers and assemble RGB array
+		r = Number("0x" + r);
+		g = Number("0x" + g);
+		b = Number("0x" + b);
+		
+		color = [r,g,b];
+	} else if(typeof color != "object" || !color.length || color.length < 3) {
+		// if color is an object but not an array with at least 3 entries, also return false
+		return false;
+	}
+	
+	// compute average of three components and return
+	return (color[0] + color[1] + color[2]) / 3;
 }
 
 // convert date to pixels considering start date and zoom factor
@@ -41,14 +74,21 @@ function Event(id, date, title, description = "", color = "#990000") {
 	this.draw = function(context, vOffset = 0) {
 		// x position = date offset in px + left canvas padding
 		let x = getPx(date) + timeline.padding.left;
-		let datestr = date.toLocaleDateString();
+		let datestr = date.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric'});
 		context.fillStyle = color;
 		// draw line to mark on timeline: upper left corner x position and top padding + vOffset, width 1, height = canvas height - padding - vOffset
-		context.fillRect(x, timeline.padding.top + vOffset, 1, context.canvas.height - timeline.padding.bottom - timeline.padding.top -vOffset);
+		// context.fillRect(x, timeline.padding.top + vOffset, 1, context.canvas.height - timeline.padding.bottom - timeline.padding.top -vOffset);
 		context.font = "14px sans-serif";
 		context.fillText(title, x-(context.measureText(title).width / 2), timeline.padding.top - 10 + vOffset);
 		context.font = "10px sans-serif";
 		context.fillText(datestr, x-(context.measureText(datestr).width / 2), timeline.padding.top - 25 + vOffset);
+		
+		// add description
+		context.font = "10px sans-serif";
+		context.fillText(description, x-(context.measureText(description).width / 2), timeline.padding.top + 5 + vOffset);
+		
+		// line has to be drawn different due to description
+		context.fillRect(x, timeline.padding.top + vOffset + 10, 1, context.canvas.height - timeline.padding.bottom - timeline.padding.top -vOffset -10);
 	};
 }
 
@@ -74,7 +114,41 @@ function newEvent() {
 	draw();
 }
 
-// invoked when deleting event from GUI
+// invoked when creating timespan from GUI
+function newTimespan() {
+	let id = prompt("Please enter an ID");
+	if(id.trim().length < 1) {
+		// failsafe, ID-less events cannot be deleted
+		alert("Could not create event! No ID given.");
+		return;
+	}
+	let title = prompt("Please enter a title");
+	let description = prompt("Please enter a description");
+	let start_date = prompt("Please enter a start date (YYYY-MM-DD)");
+	if(start_date.trim().length < 1) {
+		// failsafe, empty date defaults to 1970-01-01 otherwise
+		alert("Could not create event! No date given.");
+		return;
+	}
+	let end_date = prompt("Please enter an end date (YYYY-MM-DD)");
+	if(end_date.trim().length < 1) {
+		// failsafe, empty date defaults to 1970-01-01 otherwise
+		alert("Could not create event! No date given.");
+		return;
+	}
+	let color = prompt("Please pick a color", "#990000");
+	let layer = prompt("Please designate a layer", 0);
+	if(Number(layer) == NaN || Number(layer) == undefined || Number(layer) == null) {
+		layer = 0;
+	} else {
+		layer = Number(layer);
+	}
+	let timespan = new Timespan(id, new Date(start_date), new Date(end_date), title, description, color, layer);
+	timeline.timespans.push(timespan);
+	draw();
+}
+
+// invoked when deleting event from GUI (also works for timespans)
 function deleteEvent() {
 	let id = prompt("Please enter the ID you wish to delete");
 	let count = 0;
@@ -95,19 +169,126 @@ function deleteEvent() {
 
 // comparator function for sorting purposes, sort Event instances by date
 function compareEvents(first, second) {
+	
+	// sort timespans to the end
+	if(first.start_date || second.start_date) {
+		return 1;
+	}
+	
+	//console.log(`Comparing ${first.title} (${first.date.toLocaleString()}) and ${second.title} (${second.date.toLocaleString()})`);
+
+	if(first.date.getTime() == second.date.getTime()) {
+		//console.log("Both are identical.");
+		return 0;
+	}
+	if(first.date.getTime() > second.date.getTime()) {
+		//console.log(`${first.title} comes later!`);
+		return 1;
+	}
+	if(first.date.getTime() < second.date.getTime()) {
+		//console.log(`${second.title} comes later!`);
+		return -1;
+	}
+	
+	return 0;
+	
+	/*
 	let returnvalue = 0;
 	// 0 = events equal, second < first = 1, first < second = -1
 	first.date == second.date ? returnvalue = 0 : first.date > second.date ? returnvalue = 1 : returnvalue = -1;
 	return returnvalue;
+	*/
 }
+
+
+// class Timespan (constructor)
+function Timespan(id, start_date, end_date, title, description = "", color = "#990000", layer=0) {
+	this.id = id;
+	this.start_date = start_date;
+	this.end_date = end_date;
+	this.title = title;
+	this.description = description;
+	this.color = color;
+	this.layer = layer;
+	
+	// draw to given canvas context. positive vOffset = lower
+	this.draw = function(context, vOffset = 0) {
+		
+		// get canvas, required to draw at the bottom
+		let canvas = context.canvas;
+		
+		// x position = date offset in px + left canvas padding
+		let x1 = getPx(start_date) + timeline.padding.left;
+		let x2 = getPx(end_date) + timeline.padding.left;
+		
+				
+		let datestr1 = start_date.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric'});
+		let datestr2 = end_date.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric'});
+		
+		// set color and make rectangle half transparent
+		context.fillStyle = color;
+		context.globalAlpha = 0.5;
+		
+		// draw rectangle
+		context.fillRect(x1, canvas.height - timeline.padding.bottom - 100 - 30 - layer*GLOBAL_OFFSET_STEP, x2-x1, 30);
+		
+		// reset alpha to 1 again afterwards
+		context.globalAlpha = 1;
+		
+		
+		// set font and text color (black or white depending on rectangle background)
+		context.font = "14px sans-serif";
+		context.fillStyle = colorStrength(color) > 32 ? "black" : "white";
+		
+		// text position: half the width of the span, adjust for half text width to center. text is 14px tall, 30 - 14 = 16, divided by 2 = 8 px from the top edge
+		let titleX = x1+((x2-x1)/2) - (context.measureText(title).width / 2);
+		
+		// measure text height, subtract from rectangle. divide by two and add back text height (text is drawn upwards from lower left corner, canvas Y coordinates start at 0=top edge)
+		let titleY = (30 - context.measureText(title).emHeightAscent) / 2 + context.measureText(title).emHeightAscent;
+		
+		// draw the fucking shit
+		context.fillText(title, titleX, canvas.height - timeline.padding.bottom - 100 - 30 - layer*GLOBAL_OFFSET_STEP + titleY);
+		
+		/* drawing description and dates later because i can't be fucking arsed right now */
+		
+		/*
+		context.font = "10px sans-serif";
+		context.fillText(datestr, x-(context.measureText(datestr).width / 2), timeline.padding.top - 25 + vOffset);
+		
+		// add description
+		context.font = "10px sans-serif";
+		context.fillText(description, x-(context.measureText(description).width / 2), timeline.padding.top + 5 + vOffset);
+		
+		// line has to be drawn different due to description
+		context.fillRect(x, timeline.padding.top + vOffset + 10, 1, context.canvas.height - timeline.padding.bottom - timeline.padding.top -vOffset -10);
+		*/
+	};
+}
+
+// update files from old versions to facilitate loading
+function compatibility(loadedTimeline) {
+
+	// no title field? no problem
+	if(loadedTimeline.title == null || loadedTimeline.title == undefined) {
+		loadedTimeline.title = "";
+	}
+	
+	if(loadedTimeline.timespans == null || loadedTimeline.timespans == undefined) {
+		loadedTimeline.timespans = [];
+	}
+	
+	return loadedTimeline;
+}
+
 
 // export timeline events to JSON
 function exportEvents() {
+	timeline.events = timeline.events.sort(compareEvents);
 	let str = JSON.stringify(timeline, null, 4);
 	// create invisible download link, click it, delete it
 	let anchor = document.createElement("a");
 	anchor.setAttribute("href", "data:application/json;charset=utf-8," + encodeURIComponent(str));
-	anchor.setAttribute("download", "events.json");
+	anchor.setAttribute("download", elem("timelineTitle").value == "" ? "events.json" : elem("timelineTitle").value + ".json");
 	anchor.style.display = "none";
 	document.body.appendChild(anchor);
 	anchor.click();
@@ -129,18 +310,37 @@ function importEvents() {
 		let imported = event.target.result;
 		newTimeline = JSON.parse(imported);
 		
+		newTimeline = compatibility(newTimeline);
+		
+		timeline.title = newTimeline.title;
 		timeline.padding = newTimeline.padding;
 		timeline.zoom = newTimeline.zoom;
 		timeline.offset = newTimeline.offset;
 		timeline.end = newTimeline.end;
 		timeline.events = [];
-		for(var i = 0; i < newTimeline.events.length; i++) {
+		timeline.timespans = [];
+		for(let i = 0; i < newTimeline.events.length; i++) {
 			let event = newTimeline.events[i];
-			timeline.events.push(new Event(event.id, new Date(event.date), event.title, event.description, event.color));
+			
+			if(event.date) {
+				timeline.events.push(new Event(event.id, new Date(event.date), event.title, event.description, event.color));
+			} else if(event.start_date) {
+				timeline.timespans.push(new Timespan(event.id, new Date(event.start_date), new Date(event.end_date), event.title, event.description, event.color, event.layer));
+			}
+		}
+		for(let i = 0; i < newTimeline.timespans.length; i++) {
+			let event = newTimeline.timespans[i];
+			
+			if(event.date) {
+				timeline.events.push(new Event(event.id, new Date(event.date), event.title, event.description, event.color));
+			} else if(event.start_date) {
+				timeline.timespans.push(new Timespan(event.id, new Date(event.start_date), new Date(event.end_date), event.title, event.description, event.color, event.layer));
+			}
 		}
 		elem("startYear").value = timeline.offset;
 		elem("endYear").value = timeline.end;
 		elem("zoomFactor").value = timeline.zoom;
+		elem("timelineTitle").value = timeline.title;
 		draw();
 	};
 	reader.onerror = error => reject(error);
@@ -161,6 +361,7 @@ function draw() {
 	timeline.events = timeline.events.sort(compareEvents);
 	
 	// set zoom, start year, end year, to values selected in slider / inputs
+	timeline.title = elem("timelineTitle").value;
 	timeline.zoom = elem("zoomFactor").valueAsNumber;
 	timeline.offset = elem("startYear").valueAsNumber;
 	timeline.end = elem("endYear").valueAsNumber;
@@ -206,26 +407,30 @@ function draw() {
 		context.fillRect(x, timeline.padding.top, 2, fromBottom - timeline.padding.top);
 	}
 	
-	// 4 event long cycle of different y offset to ensure readability and avoid overlap
-	// 1st event: y offset 0, 2/3/4th event each 30px more
+	// 5 event long cycle of different y offset to ensure readability and avoid overlap
+	// 1st event: y offset 0, 2/3/4/5th event each 50px (or whatever GLOBAL_OFFSET_STEP says) more
 	// draw events
-	for(var i = 0; i < timeline.events.length; i++) {
+	for(let i = 0; i < timeline.events.length; i++) {
 		let vOffset = -1;
-		switch((i+1) % 4) {
+		switch((i+1) % 5) {
 			case 1:
-				vOffset = 0;
+				vOffset = GLOBAL_OFFSET_STEP * 0;
 				break;
 			case 2:
-				vOffset = 30;
+				vOffset = GLOBAL_OFFSET_STEP * 1;
 				break;
 			case 3:
-				vOffset = 60;
+				vOffset = GLOBAL_OFFSET_STEP * 2;
 				break;
 			case 4:
-				vOffset = 90;
+				vOffset = GLOBAL_OFFSET_STEP * 3;
+				break;
+			case 5:
+			case 0:
+				vOffset = GLOBAL_OFFSET_STEP * 4;
 				break;
 			default:
-				vOffset = -30;
+				vOffset = GLOBAL_OFFSET_STEP * (-1);
 				break;
 		}
 		
@@ -234,4 +439,6 @@ function draw() {
 		
 		timeline.events[i].draw(context, vOffset);
 	}
+	
+	timeline.timespans.forEach(timespan => { timespan.draw(context); });
 }
